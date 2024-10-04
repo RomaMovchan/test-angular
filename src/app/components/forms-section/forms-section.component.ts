17a596bcc4030462fb42a64f360e836923787c37
Roman.Movchan <r.movchan@deloitte.com>
2024-10-04 15:58:01 +0300

    init

---
+++ src/app/components/forms-section/forms-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './forms-section.component.html',
+  styleUrl: './forms-section.component.scss'
+})
+export class FormsSectionComponent {
+
+}
