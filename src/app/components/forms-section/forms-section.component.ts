9f5fac197ff8c87b59c26a7733ec83574ca02b4a
Roman.Movchan <r.movchan@deloitte.com>
2024-10-04 21:13:27 +0300

    add some components

--- src/app/components/forms-section/forms-section.component.ts
+++ src/app/components/forms-section/forms-section.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from "@angular/forms";
+import {NgForOf} from "@angular/common";
+import {FormItemComponent} from "../form-item/form-item.component";
+
+type TestForm = FormGroup<{
+  country: FormControl<string>;
+  username: FormControl<string>;
+  birthday: FormControl<string>;
+}>;
 
 @Component({
   selector: 'app-forms-section',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgForOf, FormItemComponent],
   templateUrl: './forms-section.component.html',
-  styleUrl: './forms-section.component.scss'
+  styleUrl: './forms-section.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FormsSectionComponent {
+  private fb: FormBuilder = inject(FormBuilder);
+  public formArray = new FormArray<TestForm>([]);
+
+  private registerFormGroup(): FormGroup {
+    return <TestForm>this.fb.group({
+      country: ['', [
+        Validators.required,
+      ]],
+      username: ['', [
+        Validators.required,
+      ]],
+      birthday: ['', [
+        Validators.required,
+      ]]
+    })
+  }
+
+  public getFormGroup(control: AbstractControl): FormGroup {
+    return control as FormGroup;
+  }
+
+  public addItem(): void {
+    this.formArray.push(this.registerFormGroup());
+  }
+
+  public handleRemoveItem(index: number): void {
+    this.formArray.removeAt(index);
+  }
 
 }
